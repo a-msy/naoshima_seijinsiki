97cc9e0347418626a11ce17c9548b27190fa192a
masaya <m5s2y5a@gmail.com>
2019-12-13 00:17:57 +0900

    12/13 0:17

--- index.php
+++ index.php
@@ -12,153 +12,199 @@
     <link rel="stylesheet" href="css/slider.css"/>
 </head>
 <body>
+<div id="site-content">
+    <!-- Site Name -->
+    <!-- Static navbar -->
+    <header class="clearfix">
+        <div class="container">
+            <div class="header-left">
+                <h1>2020年直島町成人式</h1>
+            </div>
+            <div class="header-right">
+                <label for="open">
+                    <span class="hidden-desktop"></span>
+                </label>
+                <input type="checkbox" name="" id="open">
+                <nav>
+                    <a href="#site-content">Home</a>
+                    <a href="#">すごいリンク</a>
+                    <a href="#">なんかみれる</a>
+                    <a href="#">ここもみれる</a>
+                    <a href="#site-footer">製作者</a>
+                </nav>
+            </div>
+        </div>
+    </header>
 
-<!-- Site Name -->
-<!-- Static navbar -->
-<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#"><h1>すごい良さげなサイトタイトル</h1></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto text_align_center">
-        </ul>
-        <ul class="navbar-nav text_align_center">
-            <li class="nav-item active m-3">
-                <a class="nav-link" href="#">
-                    <i class="fa fa-home"></i>
-                    <h3>トップ</h3>
-                    <span class="sr-only">(current)</span>
-                </a>
-            </li>
-            <li class="nav-item m-3">
-                <a class="nav-link" href="#">
-                    <i class="fa fa-envelope-o">
-                        <span class="badge badge-danger">7+</span>
-                    </i>
-                    <h3>リンク１</h3>
-                </a>
-            </li>
-            <li class="nav-item m-3">
-                <a class="nav-link" href="#">
-                    <i class="fa fa-bell">
-                        <span class="badge badge-info">7+</span>
-                    </i>
-                    <h3>きれいな通知</h3>
-                </a>
-            </li>
-        </ul>
+    <!--トップ画像-->
+    <div class="box-top">
+        <!-- // 画面フルサイズの要素 // 中央寄せされるコンテンツ -->
+        <img class="top_picture" src="https://goo.gl/gU9Bbr">
+        <div class="top_text">
+            <h1>令和2年</h1>
+            <h1>2020年</h1>
+            <h1>成人式用のオシャレなテキスト</h1>
+        </div>
     </div>
-</nav>
+    <div class="slider-container">
+        <div class="slider-control left inactive"></div>
+        <div class="slider-control right"></div>
+        <ul class="slider-pagi"></ul>
+        <div class="slider">
+            <div class="slide slide-0 active">
+                <div class="slide__bg"></div>
+                <div class="slide__content">
+                    <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+                        <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405"/>
+                    </svg>
+                    <div class="slide__text">
+                        <h2 class="slide__text-heading">かなりすごくて驚く見出し</h2>
+                        <p class="slide__text-desc">りっちゃんは、お母さんが病気なので、
+                            何かいいことをしてあげたいと思いました。
+                            ｢肩を叩いてあげようかな。
+                            なぞなぞごっこをしてあげようかな。
+                            くすぐって笑わせてあげようかな。
+                            でも、もっともっといいことはないかしら。
+                            お母さんが、たちまち
+                            元気になってしまうようなこと。｣
+                            りっちゃんは、一生懸命考えました。</p>
+                        <a class="slide__text-link">度を越したリンク</a>
+                    </div>
+                </div>
+            </div>
+            <div class="slide slide-1 ">
+                <div class="slide__bg"></div>
+                <div class="slide__content">
+                    <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+                        <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405"/>
+                    </svg>
+                    <div class="slide__text">
+                        <h2 class="slide__text-heading">かなりすごい見出し</h2>
+                        <p class="slide__text-desc">｢あっ、そうだわ。
+                            おいしいサラダをつくってあげよう。
+                            元気になるサラダをつくってあげよう。｣
 
-<!--トップ画像-->
-<div class="box-top">
-    <!-- // 画面フルサイズの要素 // 中央寄せされるコンテンツ -->
-    <img class="top_picture" src="https://goo.gl/gU9Bbr">
-    <div class="top_text">
-        <h1>令和2年</h1>
-        <h1>2020年</h1>
-        <h2>成人式用のオシャレなテキスト</h2>
-        <button></button>
+                            りっちゃんは、冷蔵庫を開けて中を覗きました。
+
+                            りっちゃんは、サラダを作り始めました。
+                            きゅうりをトントントン、キャベツは
+                            シャシャシャキ、トマトもストントントンと
+                            きって、大きなお皿にのせました。</p>
+                        <a class="slide__text-link">やばめなリンク</a>
+                    </div>
+                </div>
+            </div>
+            <div class="slide slide-2">
+                <div class="slide__bg"></div>
+                <div class="slide__content">
+                    <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+                        <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405"/>
+                    </svg>
+                    <div class="slide__text">
+                        <h2 class="slide__text-heading">ぎゅっとした見出し</h2>
+                        <p class="slide__text-desc">すると、のら猫が、こっそり入ってきて言いました。
+                            ｢サラダに鰹節を入れるといいですよ。
+                            すぐに元気になりますよ。
+                            木登りだって上手になりますよ。
+                            猫みたいにね。｣
+                            ｢教えてくれてありがとう。｣</p>
+                        <a class="slide__text-link">リンクのようなリンク</a>
+                    </div>
+                </div>
+            </div>
+            <div class="slide slide-3">
+                <div class="slide__bg"></div>
+                <div class="slide__content">
+                    <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+                        <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405"/>
+                    </svg>
+                    <div class="slide__text">
+                        <h2 class="slide__text-heading">どことなくすごい見出し</h2>
+                        <p class="slide__text-desc">りっちゃんは、さっそく鰹節をサラダにかけました。
+                            そこへ、隣の犬が飛び込んできました。
+                            ｢なんといっても、ハムサラダが一番さ。
+                            これを食べると、ほっぺたがたちまち
+                            ももいろにひかりだす。ハムみたいにね。｣
+                            ｢教えてくれてありがとう。｣</p>
+                        <a class="slide__text-link">押してみたくなるリンク</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="yabai_glid">
+        <section class="strips">
+            <article class="strips__strip">
+                <div class="strip__content">
+                    <h1 class="strip__title" data-name="Lorem">Awesome</h1>
+                    <div class="strip__inner-text">
+                        <h2>Ettrics</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia sapiente deserunt
+                            consectetur,
+                            quod reiciendis corrupti quo ea aliquid! Repellendus numquam quo, voluptate. Suscipit soluta
+                            omnis quibusdam facilis, illo voluptates odit!</p>
+                    </div>
+
+                </div>
+            </article>
+            <article class="strips__strip">
+                <div class="strip__content">
+                    <h1 class="strip__title" data-name="Ipsum">Words</h1>
+                    <div class="strip__inner-text">
+                        <h2>Ettrics</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia sapiente deserunt
+                            consectetur,
+                            quod reiciendis corrupti quo ea aliquid! Repellendus numquam quo, voluptate. Suscipit soluta
+                            omnis quibusdam facilis, illo voluptates odit!</p>
+                    </div>
+                </div>
+            </article>
+            <article class="strips__strip">
+                <div class="strip__content">
+                    <h1 class="strip__title" data-name="Dolor">Go</h1>
+                    <div class="strip__inner-text">
+                        <h2>Ettrics</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia sapiente deserunt
+                            consectetur,
+                            quod reiciendis corrupti quo ea aliquid! Repellendus numquam quo, voluptate. Suscipit soluta
+                            omnis quibusdam facilis, illo voluptates odit!</p>
+                    </div>
+                </div>
+            </article>
+            <article class="strips__strip">
+                <div class="strip__content">
+                    <h1 class="strip__title" data-name="Sit">そこ</h1>
+                    <div class="strip__inner-text">
+                        <h2>すごすぎるタイトル文</h2>
+                        <p>心を揺さぶってくるテキスト．これを読まずには死ねない．心を揺さぶってくるテキスト．これを読まずには死ねない．
+                            心を揺さぶってくるテキスト．これを読まずには死ねない．心を揺さぶってくるテキスト．これを読まずには死ねない．
+                            心を揺さぶってくるテキスト．これを読まずには死ねない．心を揺さぶってくるテキスト．これを読まずには死ねない．
+                            心を揺さぶってくるテキスト．これを読まずには死ねない．
+                        </p>
+                    </div>
+                </div>
+            </article>
+            <article class="strips__strip">
+                <div class="strip__content">
+                    <h1 class="strip__title" data-name="Amet">ここ</h1>
+                    <div class="strip__inner-text">
+                        <h2>めっちゃやばいタイトル</h2>
+                        <p>なんかすごそうなことを書いているテキスト．読むと幸せになれる．なんかすごそうなことを書いているテキスト．読むと幸せになれる．
+                            なんかすごそうなことを書いているテキスト．読むと幸せになれる．なんかすごそうなことを書いているテキスト．読むと幸せになれる．
+                            なんかすごそうなことを書いているテキスト．読むと幸せになれる．なんかすごそうなことを書いているテキスト．読むと幸せになれる．
+                            なんかすごそうなことを書いているテキスト．読むと幸せになれる．
+                        </p>
+                    </div>
+                </div>
+            </article>
+            <i class="fa fa-close strip__close"></i>
+        </section>
     </div>
 </div>
-<div class="slider-container">
-    <div class="slider-control left inactive"></div>
-    <div class="slider-control right"></div>
-    <ul class="slider-pagi"></ul>
-    <div class="slider">
-        <div class="slide slide-0 active">
-            <div class="slide__bg"></div>
-            <div class="slide__content">
-                <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
-                    <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405"/>
-                </svg>
-                <div class="slide__text">
-                    <h2 class="slide__text-heading">かなりすごくて驚く見出し</h2>
-                    <p class="slide__text-desc">りっちゃんは、お母さんが病気なので、
-                        何かいいことをしてあげたいと思いました。
-                        ｢肩を叩いてあげようかな。
-                        なぞなぞごっこをしてあげようかな。
-                        くすぐって笑わせてあげようかな。
-                        でも、もっともっといいことはないかしら。
-                        お母さんが、たちまち
-                        元気になってしまうようなこと。｣
-                        りっちゃんは、一生懸命考えました。</p>
-                    <a class="slide__text-link">度を越したリンク</a>
-                </div>
-            </div>
-        </div>
-        <div class="slide slide-1 ">
-            <div class="slide__bg"></div>
-            <div class="slide__content">
-                <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
-                    <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405"/>
-                </svg>
-                <div class="slide__text">
-                    <h2 class="slide__text-heading">かなりすごい見出し</h2>
-                    <p class="slide__text-desc">｢あっ、そうだわ。
-                        おいしいサラダをつくってあげよう。
-                        元気になるサラダをつくってあげよう。｣
-
-                        りっちゃんは、冷蔵庫を開けて中を覗きました。
-
-                        りっちゃんは、サラダを作り始めました。
-                        きゅうりをトントントン、キャベツは
-                        シャシャシャキ、トマトもストントントンと
-                        きって、大きなお皿にのせました。</p>
-                    <a class="slide__text-link">やばめなリンク</a>
-                </div>
-            </div>
-        </div>
-        <div class="slide slide-2">
-            <div class="slide__bg"></div>
-            <div class="slide__content">
-                <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
-                    <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405"/>
-                </svg>
-                <div class="slide__text">
-                    <h2 class="slide__text-heading">ぎゅっとした見出し</h2>
-                    <p class="slide__text-desc">すると、のら猫が、こっそり入ってきて言いました。
-                        ｢サラダに鰹節を入れるといいですよ。
-                        すぐに元気になりますよ。
-                        木登りだって上手になりますよ。
-                        猫みたいにね。｣
-                        ｢教えてくれてありがとう。｣</p>
-                    <a class="slide__text-link">リンクのようなリンク</a>
-                </div>
-            </div>
-        </div>
-        <div class="slide slide-3">
-            <div class="slide__bg"></div>
-            <div class="slide__content">
-                <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
-                    <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405"/>
-                </svg>
-                <div class="slide__text">
-                    <h2 class="slide__text-heading">どことなくすごい見出し</h2>
-                    <p class="slide__text-desc">りっちゃんは、さっそく鰹節をサラダにかけました。
-                        そこへ、隣の犬が飛び込んできました。
-                        ｢なんといっても、ハムサラダが一番さ。
-                        これを食べると、ほっぺたがたちまち
-                        ももいろにひかりだす。ハムみたいにね。｣
-                        ｢教えてくれてありがとう。｣</p>
-                    <a class="slide__text-link">押してみたくなるリンク</a>
-                </div>
-            </div>
-        </div>
-    </div>
+<div id="site-footer">
+    <h1>©Copyright Imada Masaya <a href="https://alterrucks.com">alterrucks.com</a></h1>
 </div>
-
-<h1>なんかやばいグリッドたち</h1>
-<?php
-for ($i = 1; $i <= 10; $i++) {
-    echo "<h1>" . $i . "</h1>";
-}
-?>
-
-<h1>やばめなフッター</h1>
 <!---->
 <!--javascript-->
 <script src="https://kit.fontawesome.com/bc7e8c6689.js" crossorigin="anonymous"></script>
@@ -174,4 +220,3 @@ for ($i = 1; $i <= 10; $i++) {
 <script src="js/index.js"></script>
 </body>
 </html>
-?>
--- js/index.js
+++ js/index.js
@@ -121,3 +121,99 @@ $(document).ready(function() {
     });
 
 });
+
+var Expand = (function() {
+    var tile = $('.strips__strip');
+    var tileLink = $('.strips__strip > .strip__content');
+    var tileText = tileLink.find('.strip__inner-text');
+    var stripClose = $('.strip__close');
+
+    var expanded  = false;
+
+    var open = function() {
+
+        var tile = $(this).parent();
+
+        if (!expanded) {
+            tile.addClass('strips__strip--expanded');
+            // add delay to inner text
+            tileText.css('transition', 'all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)');
+            stripClose.addClass('strip__close--show');
+            stripClose.css('transition', 'all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)');
+            expanded = true;
+        }
+    };
+
+    var close = function() {
+        if (expanded) {
+            tile.removeClass('strips__strip--expanded');
+            // remove delay from inner text
+            tileText.css('transition', 'all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)');
+            stripClose.removeClass('strip__close--show');
+            stripClose.css('transition', 'all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)')
+            expanded = false;
+        }
+    }
+
+    var bindActions = function() {
+        tileLink.on('click', open);
+        stripClose.on('click', close);
+    };
+
+    var init = function() {
+        bindActions();
+    };
+
+    return {
+        init: init
+    };
+
+}());
+
+Expand.init();
+
+$(document).ready(function(){
+
+    (function($) {
+
+        $('#header__icon').click(function(e){
+            e.preventDefault();
+            $('body').toggleClass('with--sidebar');
+        });
+
+        $('#site-cache').click(function(e){
+            $('body').removeClass('with--sidebar');
+        });
+
+    })(jQuery);
+
+});
+
+$(document).ready(function() {
+
+    // INITIATE THE FOOTER
+    siteFooter();
+    // COULD BE SIMPLIFIED FOR THIS PEN BUT I WANT TO MAKE IT AS EASY TO PUT INTO YOUR SITE AS POSSIBLE
+    $(window).resize(function() {
+        siteFooter();
+    });
+
+    function siteFooter() {
+        var siteContent = $('#site-content');
+        var siteContentHeight = siteContent.height();
+        var siteContentWidth = siteContent.width();
+
+        var siteFooter = $('#site-footer');
+        var siteFooterHeight = siteFooter.height();
+        var siteFooterWidth = siteFooter.width();
+
+        console.log('Content Height = ' + siteContentHeight + 'px');
+        console.log('Content Width = ' + siteContentWidth + 'px');
+        console.log('Footer Height = ' + siteFooterHeight + 'px');
+        console.log('Footer Width = ' + siteFooterWidth + 'px');
+
+        siteContent.css({
+            "margin-bottom" : siteFooterHeight + 50
+        });
+    };
+});
